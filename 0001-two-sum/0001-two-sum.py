71e73ee304804a3b3e1883079857279236cd465f
shashank yadav <71840258+shashank1234751@users.noreply.github.com>
2026-07-27 18:48:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,3 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        seen={}
-        for n ,nums in enumerate(nums):
-            need=target-nums
-            if need in seen:
-                return [n,seen[need]]
-            seen[nums]=n
-        return None
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
